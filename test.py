705e53eb57d05859694e285ebee126ae60a0c693
wonjun-kim <glistering96@gmail.com>
2023-06-08 17:13:32 +0900

    official example

--- test.py
+++ test.py
@@ -1,26 +1,20 @@
-from datawig.simple_imputer import SimpleImputer
-import pandas as pd
-import numpy as np
-import mxnet as mx
+import datawig
 
+df = datawig.utils.generate_df_string( num_samples=200, 
+                                       data_column_name='sentences', 
+                                       label_column_name='label')
 
-if __name__ == "__main__":
-    # generate a dataset with missing values for debugging using pandas and numpy
-    df = pd.DataFrame(np.random.randn(10, 3), columns=list('ABC'))
-    df.loc[df.sample(frac=0.1).index, 'A'] = np.nan
-    df.loc[df.sample(frac=0.1).index, 'B'] = np.nan
-    df.loc[df.sample(frac=0.1).index, 'C'] = np.nan
-    df.head()
-    
-    # initialize a SimpleImputer model
-    imputer = SimpleImputer(
-        input_columns=['B', 'C'],  # columns containing information about the column we want to impute
-        output_column='A',  # the column we'd like to impute values for
-        output_path='imputer_model'  # stores model data and metrics
+df_train, df_test = datawig.utils.random_split(df)
+
+#Initialize a SimpleImputer model
+imputer = datawig.SimpleImputer(
+    input_columns=['sentences'], # column(s) containing information about the column we want to impute
+    output_column='label', # the column we'd like to impute values for
+    output_path = 'imputer_model' # stores model data and metrics
     )
-    
-    # fit an imputer model on the train data using gpu. Set the context to gpu.
-    context = mx.gpu(0)
-    
-    imputer.fit(train_df=df, num_epochs=1, ctx=context)
-    
+
+#Fit an imputer model on the train data
+imputer.fit(train_df=df_train)
+
+#Impute missing values and return original dataframe with predictions
+imputed = imputer.predict(df_test)
